c00914e8953f52ab06ee46fb58fc3c4bfb88b4f2
Enziaki <58030340+enziaki@users.noreply.github.com>
2023-06-05 20:16:27 +0530

    Working Alpha patch added

--- config.mk
+++ config.mk
@@ -1,4 +1,4 @@
-# dwm version
+#dwm version
 VERSION = 6.4
 
 # Customize below to fit your system
@@ -24,7 +24,7 @@ FREETYPEINC = /usr/include/freetype2
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC}
-LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lX11-xcb -lxcb -lxcb-res ${KVMLIB}
+LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lX11-xcb -lxcb -lxcb-res -lXrender ${KVMLIB} 
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
